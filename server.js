1fb8799c65db4c45c286a4d488ccaf8e0d0c7c44
Nama Pengguna Anda <email@domain.com>
2025-10-09 21:35:41 +0700

    update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // MongoDB Connection
